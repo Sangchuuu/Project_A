520a43e099581cb4eb783d0d9c998d29757027e6
LDHEBONY <81902592+LDHEBONY@users.noreply.github.com>
2022-10-21 15:30:14 +0900

    monsterai01_0.115
    작업내역저장

--- Proj_A/Assets/1_Script/MonsterAI01.cs
+++ Proj_A/Assets/1_Script/MonsterAI01.cs
@@ -7,22 +7,26 @@ using UnityEngine.AI;
 public class MonsterAI01 : MonoBehaviour
 {
 
-    NavMeshAgent agent;
+    //NavMeshAgent agent;
 
     public enum m_state { IDLE, PATROL, CHASE, MOVE };
+
+    int r_indexnumber = Random.Range(0, 2);
+
+    [SerializeField]
+    Transform[] m_targetsarray = new Transform[15];
 
     [SerializeField]
     Transform target;
     Transform WayPoint01;
-
+    Transform WayPoint02;
+    Transform WayPoint03;
     
 
     private void Awake()
     {      
-        agent = GetComponent<NavMeshAgent>();
+        //agent = GetComponent<NavMeshAgent>();
     }
-
-   
 
     // Start is called before the first frame update
     void Start()
@@ -33,7 +37,7 @@ public class MonsterAI01 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.position);
+        //UpdateState();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -42,6 +46,39 @@ public class MonsterAI01 : MonoBehaviour
         {
 
         }
+    }
+
+    void WaypointIndex()
+    {
+        WayPoint01 = m_targetsarray[0];
+        WayPoint02 = m_targetsarray[1];
+        WayPoint03 = m_targetsarray[2];
+    }
+
+    void UpdateState(m_state m_state)
+    {
+        switch (m_state) 
+        {
+            case m_state.IDLE:
+                m_state = m_state.MOVE;
+                break;
+            case m_state.MOVE:
+                MoveMonster();
+                break;
+            case m_state.PATROL:
+                break;
+            case m_state.CHASE:
+                break;
+            default:
+                m_state = m_state.IDLE;
+                break;       
+        }
+
+    }
+
+    void MoveMonster()
+    {
+        //agent.SetDestination(m_targetsarray[r_indexnumber].position);
     }
 
 }
