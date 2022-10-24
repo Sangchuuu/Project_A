aeb3f3cd198a65233bae74441052ce989336162f
Sangchuuu <wjdgns2k@gmail.com>
2022-10-24 20:51:10 +0900

    몬스터 AI(진행중)
    리스폰 포인트, 몬스터 AI 진행상황 업데이트

--- Proj_A/Assets/1_Script/WayPoint.cs
+++ Proj_A/Assets/1_Script/WayPoint.cs
@@ -5,16 +5,53 @@ using UnityEngine;
 public class WayPoint : MonoBehaviour
 {
     [SerializeField]
-    private Transform[] wayPoints;
+    List<GameObject> m_listWayPoints = new List<GameObject> ();
 
-
-    private void OnTriggerStay(Collider other)
+    
+    [SerializeField] 
+    private int m_nRandNum = 0;
+    [SerializeField]
+    private float m_fDetectLength; // 웨이포인트 감지거리. 인스펙터에서 수정하면 웨이포인트끼리 감지거리가 증가함.
+    
+    public void FindWayPoint()
     {
-        int ranPoint = Random.Range(0, 3);
-        if(other.gameObject.tag == "Player" && GameObject.Find("Player").GetComponent<User>().canTel)
+        Collider[] colliders = Physics.OverlapSphere(this.transform.position, m_fDetectLength);
+        for(int i = 0; i < colliders.Length; i++)
         {
-            other.transform.position = wayPoints[ranPoint].position;
-            
+            if (colliders[i].gameObject.CompareTag("WayPoint"))
+            {
+                if (colliders[i].gameObject != this.gameObject)
+                {
+                     m_listWayPoints.Add(colliders[i].gameObject);
+                     m_nRandNum = m_listWayPoints.Count;
+                }
+            }
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Monster"))
+        {
+            other.gameObject.GetComponent<MonsterAi>().ObjWayPoint = m_listWayPoints[Random.Range(0, m_nRandNum)];
+            other.gameObject.GetComponent<MonsterAi>().IsArrived = true;
+        }
+    }
+
+   private void OnDrawGizmos()
+   {
+       Gizmos.color = Color.red;
+       Gizmos.DrawWireSphere(this.transform.position, m_fDetectLength);
+   }
+
+
+    private void Awake()
+    {
+        m_fDetectLength = 10f;
+    }
+    private void Start()
+    {
+        FindWayPoint(); // 이거 주석처리하면 끄면 감지범위 꺼짐.
+    }
+
 }
