5b25c8651fc9648ef85f604fd3d9317b90dca626
LDHEBONY <81902592+LDHEBONY@users.noreply.github.com>
2022-10-24 15:48:06 +0900

    monsterai_0.125_01
    data stacked of failure

--- Proj_A/Assets/1_Script/MonsterAI01.cs
+++ Proj_A/Assets/1_Script/MonsterAI01.cs
@@ -9,9 +9,13 @@ public class MonsterAI01 : MonoBehaviour
 
     //NavMeshAgent agent;
 
-    public enum m_state { IDLE, PATROL, CHASE, MOVE };
+    public enum m_state { IDLE, PATROL, CHASE, MOVE, POINT };
 
-    int r_indexnumber = Random.Range(0, 2);
+    public int r_indexnumber;
+
+    [SerializeField]
+    NavMeshAgent agent;
+
 
     [SerializeField]
     Transform[] m_targetsarray = new Transform[15];
@@ -21,31 +25,52 @@ public class MonsterAI01 : MonoBehaviour
     Transform WayPoint01;
     Transform WayPoint02;
     Transform WayPoint03;
-    
+
+   public  m_state Istate;
+
+    public bool visited;
 
     private void Awake()
-    {      
-        //agent = GetComponent<NavMeshAgent>();
+    {
+        agent = GetComponent<NavMeshAgent>();
+        WaypointIndex();
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        SetState(Istate);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        //UpdateState();
+        //StartCoroutine(UpdateState());/*courtain*/
+        /*switch (Istate)
+        {
+            case m_state.IDLE:
+                r_indexnumber = Random.Range(0, 3);
+                Istate = m_state.MOVE;
+                break;
+            case m_state.MOVE:
+                MoveMonster();
+                Istate = m_state.IDLE;
+                break;
+            case m_state.PATROL:
+                break;
+            case m_state.CHASE:
+                break;
+
+        }//스테이트를 깡으로 업데이트 돌림(60프레임)*/
+
+        Debug.Log(Istate);
+        Debug.Log(visited);
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
-        {
-
-        }
+    private void FixedUpdate()
+    {       
+        UpdateState();       
     }
 
     void WaypointIndex()
@@ -55,30 +80,79 @@ public class MonsterAI01 : MonoBehaviour
         WayPoint03 = m_targetsarray[2];
     }
 
-    void UpdateState(m_state m_state)
+    void UpdateState()
     {
-        switch (m_state) 
+        
+        switch (Istate)
         {
-            case m_state.IDLE:
-                m_state = m_state.MOVE;
+            case m_state.IDLE:                               
                 break;
             case m_state.MOVE:
-                MoveMonster();
+
+                /*if (visited == false)
+                {
+                    MoveMonster();
+                    if (Vector3.Distance(m_targetsarray[r_indexnumber].transform.position, this.transform.position) == 0f)
+                    {
+                        visited = true;
+                    }
+                }
+                else
+                {                   
+                    Istate = m_state.IDLE;
+                }*/
+
+                     
+                    break;               
+            case m_state.POINT: // 웨이포인트 설정
+                SetState(m_state.MOVE);
                 break;
             case m_state.PATROL:
                 break;
             case m_state.CHASE:
                 break;
-            default:
-                m_state = m_state.IDLE;
-                break;       
+                 
         }
 
+       
     }
 
-    void MoveMonster()
+    void SetState(m_state Status)
     {
-        //agent.SetDestination(m_targetsarray[r_indexnumber].position);
+        switch (Status)
+        {
+            case m_state.IDLE:
+                SetState(m_state.POINT);
+                break;
+            case m_state.MOVE:
+                
+                break;
+            case m_state.POINT:               
+                r_indexnumber = Random.Range(0, 3);
+                agent.SetDestination(m_targetsarray[r_indexnumber].position);               
+                break;
+            case m_state.PATROL:
+                
+                break;
+            case m_state.CHASE:
+               
+                break;
+
+        }
+
+        Istate = Status;
+        
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("OnTriggerEnter:" + other.name);
+        if(other.gameObject.tag == ("Waypoint"))
+        {
+            SetState(m_state.POINT);
+        }
+    }
+
 
 }
