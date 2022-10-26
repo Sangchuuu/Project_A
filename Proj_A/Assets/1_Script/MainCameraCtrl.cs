391c64b21714bd9607e87e3b272e28c65877fa10
LDHEBONY <81902592+LDHEBONY@users.noreply.github.com>
2022-10-26 12:39:23 +0900

    monster01c_03
    카메라및플레이어코드추가

---
+++ Proj_A/Assets/1_Script/MainCameraCtrl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraCtrl : MonoBehaviour
+{
+    private float xRotate, yRotate, xRotateMove, yRotateMove;
+    public float rotateSpeed = 500.0f;
+
+    void Update()
+    {
+        //if (Input.GetMouseButton(0)) // Ŭ���� ���
+        //{
+        xRotateMove = -Input.GetAxis("Mouse Y") * Time.deltaTime * rotateSpeed;
+        yRotateMove = Input.GetAxis("Mouse X") * Time.deltaTime * rotateSpeed;
+
+        yRotate = transform.eulerAngles.y + yRotateMove;
+        //xRotate = transform.eulerAngles.x + xRotateMove; 
+
+        xRotate = xRotate + xRotateMove;
+
+        xRotate = Mathf.Clamp(xRotate, -50, 50); // ��, �Ʒ� ����
+
+        transform.eulerAngles = new Vector3(xRotate, yRotate, 0);
+        // }
+    }
+}
